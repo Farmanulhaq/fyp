8a63176f73742af9a9f18d15b2d871785b02d085
hassam <hassam@hassam.com>
2022-05-22 17:40:29 +0500

    initial file struc

--- fyp/src/App.js
+++ fyp/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import './index.js';
+import React from 'react'
+import {Article, Brand, Cta, Features} from "./components"
 
-
-function App() {
-  
+const App = () => {
+  return (
+    <div>GPT-3</div>
+  )
 }
 
-
-export default App;
+export default App
---
+++ fyp/src/containers/aboutBC/AboutBC.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./aboutBC.css"
+const AboutBC = () => {
+  return (
+    <div>AboutBC</div>
+  )
+}
+
+export default AboutBC
