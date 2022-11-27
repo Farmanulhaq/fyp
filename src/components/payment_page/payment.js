6d324e973276430dc2db3cbee97fc593c703cd69
Hassam <hassam@hassam.com>
2022-11-27 19:39:18 +0500

    payment

---
+++ src/components/payment_page/payment.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import React, { useEffect } from 'react'
+import Footer from '../footer/Footer';
+import './payment.css'
+
+const payment = () => {
+    return(
+        <>
+  <div class="wrapper">
+        <form action="" >
+            <h4>Account</h4>
+            <div class="input_group">
+                <div class="input_box">
+                    <input type="text" placeholder="Full Name" required class="name"></input>
+                    <i class="fa fa-user icon"></i>
+                </div>
+                <div class="input_box">
+                    <input type="text" placeholder="Name on Card" required class="name"></input>
+                    <i class="fa fa-user icon"></i>
+                </div>
+            </div>
+            <div class="input_group">
+                <div class="input_box">
+                    <input type="email" placeholder="Email Address" required class="name"></input>
+                    <i class="fa fa-envelope icon"></i>
+                </div>
+            </div>
+            <div class="input_group">
+                <div class="input_box">
+                    <input type="text" placeholder="Address" required class="name"></input>
+                    <i class="fa fa-map-marker icon" aria-hidden="true"></i>
+                </div>
+            </div>
+            <div class="input_group">
+                <div class="input_box">
+                    <input type="text" placeholder="City" required class="name"></input>
+                    <i class="fa fa-institution icon"></i>
+                </div>
+            </div>
+
+
+            <div class="input_group">
+                <div class="input_box">
+                    <h4>Date Of Birth</h4>
+                    <input type="text" placeholder="DD" required class="dob"></input>
+                    <input type="text" placeholder="MM" required class="dob"></input>
+                    <input type="text" placeholder="YYYY" required class="dob"></input>
+                </div>
+                <div class="input_box">
+                    <h4>Gender</h4>
+                    <input type="radio" name="gender" class="radio" id="b1" checked></input>
+                    <label for="b1">Male</label>
+                    <input type="radio" name="gender" class="radio" id="b2"></input>
+                    <label for="b2">Female</label>
+                </div>
+            </div>
+
+
+            <div class="input_group">
+                <div class="input_box">
+                    <h4>Payment Details</h4>
+                    <input type="radio" name="pay" class="radio" id="bc1" checked></input>
+                    <label for="bc1"><span>
+                            <i class="fa fa-cc-visa"></i>Credit Card</span></label>
+                    <input type="radio" name="pay" class="radio" id="bc2"></input>
+                    <label for="bc2"><span>
+                            <i class="fa fa-cc-paypal"></i>EasyPaisa/jazzcash</span></label>
+                </div>
+            </div>
+            <div class="input_group">
+                <div class="input_box">
+                    <input type="tel" name="" class="name" placeholder="Card Number 1111-2222-3333-4444" required></input>
+                    <i class="fa fa-credit-card icon"></i>
+                </div>
+            </div>
+            <div class="input_group">
+                <div class="input_box">
+                    <input type="tel" name="" class="name" placeholder="Card CVC 632" required></input>
+                    <i class="fa fa-user icon"></i>
+                </div>
+            </div>
+            <div class="input_group">
+                <div class="input_box">
+                    <div class="input_box">
+                        <input type="number" placeholder="Exp Month" required class="name"></input>
+                        <i class="fa fa-calendar icon" aria-hidden="true"></i>
+                    </div>
+                </div>
+                <div class="input_box">
+                    <input type="number" placeholder="Exp Year" required class="name"></input>
+                    <i class="fa fa-calendar-o icon" aria-hidden="true"></i>
+                </div>
+            </div>
+            <div class="input_box">
+                <input type="number" placeholder="Enter Amount" required class="name"></input>
+                <i class="fa fa-money icon" aria-hidden="true"></i>
+            </div>
+
+            <div class="input_group">
+                <div class="input_box">
+                    <button type="submit">PAY NOW</button>
+                </div>
+            </div>
+
+        </form>
+    </div>
+        </>
+    )
+    
+
+
+}
+
+export default Payment;
