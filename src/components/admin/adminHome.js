90d5d8f5c9ff2ced100da11b1d97633ec8f32518
Sheheryar Waseem <sherishaikh6@gmail.com>
2023-01-17 17:37:54 +0500

    metamask conected

---
+++ src/components/admin/adminHome.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Footer from '../footer/Footer'
+import './adminHome.css'
+import Web3 from "web3"
+import myContract from "../../smartContract/Contract.json"
+
+import { ethers } from 'ethers'
+import { getContractAddress } from 'ethers/lib/utils'
+
+const AdminHome = () => {
+
+    const contractAddress = '0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B';
+    let nav = useNavigate();
+
+    const [chases, setchases] = useState("");
+    const [ipfs, setIpfs] = useState("");
+
+    const [ConnButtonText, setConnButtonText] = useState("Connect");
+
+    const [ErrorMessage, setErrorMessage] = useState("");
+    const [defaultAccount, setdefaultAccount] = useState("");
+
+
+    const [provider, setProvider] = useState("");
+    const [signer, setSigner] = useState("");
+    const [contract, setContract] = useState("");
+
+    const [myWallet, setMyWallet] = useState("");
+    const [myBalance, setMyBalance] = useState("");
+    const [myWeb3, setMyWeb]= useState([]);
+
+
+    // ----------------------------------------------------------------------------------------
+    const accountChangeHandler = (newAccount) => {
+        setMyWallet(newAccount)
+        updateEthers()
+    }
+    const metamask = async () => {
+        if (window.ethereum) {
+            window.ethereum.request({method : 'eth_requestAccounts'})
+            .then(result => {
+                accountChangeHandler(result[0]);
+                setConnButtonText('Connected');
+              })
+      
+            } else {
+            alert("Install Metamask");
+        }
+    }
+
+
+    const updateEthers = () => {
+        let tempProvider = new ethers.providers.Web3Provider(window.ethereum)
+        setProvider(provider)
+
+        let tempSigner = tempProvider.getSigner()
+        setSigner(tempSigner)
+
+        let tempContract = new ethers.Contract(contractAddress, myContract, tempSigner)
+        setContract(tempContract)
+    }
+    // ------------------------------------------------------------------------------------------------------------
+
+    const storeData = async (event) => {
+        console.log('hello');
+        event.preventDefault()
+        console.log(ipfs , chases)
+        contract.addData(ipfs, chases)
+    }
+    
+
+
+    // ---------------------------------------------------------------------------------------------------------------
+
+    useEffect(() => {
+        if (!localStorage.getItem("authToken")) {
+            nav('/login');
+        }
+    })
+
+    return (
+        <>
+            <div class="container">
+                <div class="l-screen">
+                    <div class="overlay">
+                        <div class="overlay-panel overlay-right">
+                            <h1 class='r1'>Vehicify Admin Home</h1>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="screen">
+                    <div class="screen__content">
+                        <form class="login">
+                            <div class="login__field">
+                                <i class="login__icon fas fa-user"></i>
+                                <input type="text" id="setChases" class="login__input" placeholder="Enter chases Number" onChange={(e) => setchases(e.target.value)} required >
+                                </input>
+                                <input type="text" id="setIpfs" class="login__input" placeholder="Enter IPFS Hash" onChange={(e) => setIpfs(e.target.value)} required >
+                                </input>
+                            </div>
+
+                            <button type='button' onClick={storeData} class="button login__submit" id="chases_submit" >
+                                <span class="button__text" >Submit</span>
+                                <i class="button__icon fas fa-chevron-right"></i>
+                            </button>
+
+                            <button type='button' class="button login__submit" id="chases_submit" onClick={metamask} >
+                                <span class="button__text" >{ConnButtonText}</span>
+                                <i class="button__icon fas fa-chevron-right"></i>
+                            </button>
+
+                        </form>
+                    </div>
+                    <div class="screen__background">
+                        <span class="screen__background__shape screen__background__shape1"></span>
+                    </div>
+
+                </div>
+
+
+            </div>
+            <Footer />
+
+        </>
+    )
+}
+
+export default AdminHome
+// onClick={() => { nav('/payment') }}
+// onClick={Store_chases_No}
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './Footer.css'
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import { FaAngleRight, FaFacebookF, FaInstagram, FaLinkedinIn, FaSkype, FaTwitter } from "react-icons/fa"
+
+export default function Footer() {
+    return (
+        <>
+            <MDBFooter bgColor='dark' id='footer'>
+
+                <div className='footer' id='footer'>
+                    <div className='footer_first' >
+                        <div className='first_text'>
+
+                            <p>Get connected with us on social networks:</p>
+                        </div>
+                        <div className='first_icons'>
+                            <a href='' className='me-4 text-reset'>
+                                <MDBIcon fab icon="facebook-f" />
+                            </a>
+                            <a href='' className='me-4 text-reset'>
+                                <MDBIcon fab icon="twitter" />
+                            </a>
+                            <a href='' className='me-4 text-reset'>
+                                <MDBIcon fab icon="google" />
+                            </a>
+                            <a href='' className='me-4 text-reset'>
+                                <MDBIcon fab icon="instagram" />
+                            </a>
+                            <a href='' className='me-4 text-reset'>
+                                <MDBIcon fab icon="linkedin" />
+                            </a>
+                            <a href='' className='me-4 text-reset'>
+                                <MDBIcon fab icon="github" />
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div className='footer_second'>
+                        <div className='company'>
+                            <h5 className='company_name text-uppercase fw-bold mb-4'>VEHICIFY </h5>
+                            <p className='company_des'>Pakistan's first Imported Car Auction Sheet Verification through Blockchain Technology.</p>
+
+                        </div>
+                        <div className='products'>
+                            <h6 className='product text-uppercase fw-bold mb-4'>Products </h6>
+                            <ul className='unorder'>
+                                <li><a href=''>Home</a></li>
+                                <li><a href=''>Verification</a></li>
+                                <li><a href=''>About Us</a></li>
+                                <li><a href=''>Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div className='contacts'>
+                            <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+                            <p>
+                                <MDBIcon icon="home" className="me-2" />
+                                Karachi, PAK
+                            </p>
+                            <p>
+                                <MDBIcon icon="envelope" className="me-3" />
+                                contact@vehicify.co
+                            </p>
+                            <p>
+                                <MDBIcon icon="phone" className="me-3" />+923 113 382 281
+                            </p>
+                            <p>
+                                <MDBIcon icon="print" className="me-3" />+923 162 828 064
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+                        <p>Made with Love in Pakistan
+                        </p>
+                        © 2022 Copyright:
+                        <a className='text-reset fw-bold' href='https://Vehicify.com/'>
+                            Vehicify.co
+                        </a>
+                    </div>
+
+                </div>
+
+            </MDBFooter>
+
+
+
+
+        </>
+    );
+}
